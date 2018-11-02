67bea360c28a621ebbe431d8bb382f3b97ad3b50
Krzysztof Miemiec <krzysztof.miemiec@gmail.com>
2018-11-02 22:08:35 +0100

    DOC: Improved chapters about fuzzy logic, research and project, added schematics Updated ResultsDOM component

--- src/renderer/views/query/components/results-dom/results-dom.component.tsx
+++ src/renderer/views/query/components/results-dom/results-dom.component.tsx
@@ -1,4 +1,3 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 import React from 'react';
 import uuid from 'uuid/v4';
 import { ResultsComponent } from '../results/results.component';
@@ -10,18 +9,18 @@ export class ResultsDom extends ResultsComponent {
 
   renderRow = (row: any[], index: number) => {
     return (
-      <TableRow key={index}>
-        <TableCell key={INDEX_KEY}>{index}</TableCell>
+      <div className={styles.tableRow} key={index}>
+        <div className={styles.tableCell} key={INDEX_KEY}>{index}</div>
         {row.map(this.renderCell)}
-      </TableRow>
+      </div>
     );
   };
 
   renderCell = (value: any, index: number) => {
     return (
-      <TableCell key={index}>
+      <div className={styles.tableCell} key={index}>
         {`${value}`}
-      </TableCell>
+      </div>
     );
   };
 
@@ -29,18 +28,18 @@ export class ResultsDom extends ResultsComponent {
     const { data, headers, query } = this.props;
     const time = Date.now();
     const result = (
-      <div className={styles.table}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell key={INDEX_KEY} />
+      <div className={styles.container}>
+        <div className={styles.table}>
+          <div className={styles.tableHeader}>
+            <div className={styles.tableRow}>
+              <div className={styles.tableCell} key={INDEX_KEY}/>
               {headers.map((header, index) => this.renderCell(header.name, index))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
+            </div>
+          </div>
+          <div className={styles.tableRows}>
             {data.map(this.renderRow)}
-          </TableBody>
-        </Table>
+          </div>
+        </div>
       </div>
     );
     if (query.end - query.start > 0) {
--- src/renderer/views/query/components/results-dom/results-dom.styles.tsx
+++ src/renderer/views/query/components/results-dom/results-dom.styles.tsx
@@ -1,12 +1,44 @@
 import { createStyles } from '../../../../app.styles';
+import { R } from '../../../../../common/resources';
+
+const border = '1px solid #999';
 
 export const styles = createStyles({
-  table: {
+  container: {
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
     overflow: 'scroll',
+  },
+  table: {
+    position: 'relative',
+    display: 'table',
+  },
+  tableHeader: {
+    display: 'table-header-group',
+    background: '#EEE',
+  },
+  tableRows: {
+    display: 'table-row-group',
+  },
+  tableRow: {
+    display: 'table-row',
+  },
+  tableHeadRow: {
+    display: 'table-header-group',
+    ['& $tableCell' as any]: {
+      borderTop: border,
+    },
+  },
+  tableCell: {
+    display: 'table-cell',
+    padding: R.dimen.spacing / 4,
+    borderBottom: border,
+    borderRight: border,
+    ['& :first' as any]: {
+      borderLeft: border,
+    },
   },
 });
